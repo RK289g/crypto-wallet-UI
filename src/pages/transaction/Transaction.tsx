6d1479d0f2fe4b0282bff3be74d210f361ccd230
Raisul Karim Saju <rksaju80@gmail.com>
2024-08-23 01:56:58 +0600

    ui edited

--- src/pages/transaction/Transaction.tsx
+++ src/pages/transaction/Transaction.tsx
@@ -3,7 +3,6 @@ import { BsThreeDots } from "react-icons/bs";
 import { FaBtc, FaEthereum } from "react-icons/fa";
 import { GiGasPump } from "react-icons/gi";
 import { IoLogoUsd } from "react-icons/io";
-import { MdOutlineCurrencyExchange } from "react-icons/md";
 import { RxHamburgerMenu } from "react-icons/rx";
 
 const Transaction = () => {
@@ -21,7 +20,7 @@ const Transaction = () => {
         <RxHamburgerMenu color="#E84F1F" fontSize={30} />
         <Box>
           <Text fz={18} ta="center" fw={600} c="#E84F1F">
-            Account Name
+            Jon Doe
           </Text>
           <Text fz={10} ta="center" c="#6c757d">
             1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa
@@ -88,27 +87,31 @@ const Transaction = () => {
               }}
             >
               <Text ta="left" fz={10} c="#3d3b3c">
-                #2-8/28/2024 at 12:40
+                #5-8/28/2024 at 12:40
               </Text>
-              <Flex justify="space-between" align="center" gap={130}>
-                <Flex justify="center" align="center" gap={20}>
-                  <MdOutlineCurrencyExchange fontSize={30} color="#ff7f11" />
-                  <Box>
-                    <Text fz={16}>Execute</Text>
-                    <Text
-                      fz={10}
-                      c="#E84F1F"
-                      bg="#F5F2E0"
-                      pl={5}
-                      style={{ borderRadius: "5px" }}
-                    >
-                      Approved
-                    </Text>
-                  </Box>
-                </Flex>
+              <Flex justify="space-between" align="center" gap={73}>
                 <Box>
-                  <Text>$ 0.00 USD</Text>
+                  <Flex align="center" gap={20}>
+                    <FaBtc fontSize={30} color="#ff7f11" />
+                    <Box>
+                      <Text fz={10} mb={5}>
+                        1A1zP1eP5QGefi2DMPTfTL
+                      </Text>
+                      <Text
+                        fz={10}
+                        c="#E84F1F"
+                        bg="#F5F2E0"
+                        ta="center"
+                        w={54}
+                        style={{ borderRadius: "5px" }}
+                      >
+                        Approved
+                      </Text>
+                    </Box>
+                  </Flex>
                 </Box>
+
+                <Text> 0.005 BTC</Text>
               </Flex>
             </Box>
             <Box
@@ -118,27 +121,30 @@ const Transaction = () => {
               }}
             >
               <Text ta="left" fz={10} c="#3d3b3c">
-                #2-8/28/2024 at 12:40
+                #2-8/27/2024 at 14:20
               </Text>
-              <Flex justify="space-between" align="center" gap={130}>
-                <Flex justify="center" align="center" gap={20}>
-                  <MdOutlineCurrencyExchange fontSize={30} color="#ff7f11" />
-                  <Box>
-                    <Text fz={16}>Execute</Text>
-                    <Text
-                      fz={10}
-                      c="#E84F1F"
-                      bg="#F5F2E0"
-                      pl={5}
-                      style={{ borderRadius: "5px" }}
-                    >
-                      Approved
-                    </Text>
-                  </Box>
-                </Flex>
+              <Flex justify="space-between" align="center" gap={80}>
                 <Box>
-                  <Text>$ 0.00 USD</Text>
+                  <Flex align="center" gap={20}>
+                    <FaEthereum fontSize={27} color="#627eea" />
+                    <Box>
+                      <Text fz={10} mb={5}>
+                        0x742d35Cc6634C053292
+                      </Text>
+                      <Text
+                        fz={10}
+                        c="#2ECC71"
+                        bg="#E0F7F5"
+                        ta="center"
+                        w={54}
+                        style={{ borderRadius: "5px" }}
+                      >
+                        Executed
+                      </Text>
+                    </Box>
+                  </Flex>
                 </Box>
+                <Text>0.152 ETH</Text>
               </Flex>
             </Box>
             <Box
@@ -148,27 +154,30 @@ const Transaction = () => {
               }}
             >
               <Text ta="left" fz={10} c="#3d3b3c">
-                #2-8/28/2024 at 12:40
+                #3-8/27/2024 at 15:10
               </Text>
-              <Flex justify="space-between" align="center" gap={130}>
-                <Flex justify="center" align="center" gap={20}>
-                  <MdOutlineCurrencyExchange fontSize={30} color="#ff7f11" />
-                  <Box>
-                    <Text fz={16}>Execute</Text>
-                    <Text
-                      fz={10}
-                      c="#E84F1F"
-                      bg="#F5F2E0"
-                      pl={5}
-                      style={{ borderRadius: "5px" }}
-                    >
-                      Approved
-                    </Text>
-                  </Box>
-                </Flex>
+              <Flex justify="space-between" align="center" gap={73}>
                 <Box>
-                  <Text>$ 0.00 USD</Text>
+                  <Flex align="center" gap={20}>
+                    <FaBtc fontSize={30} color="#ff7f11" />
+                    <Box>
+                      <Text fz={10} mb={5}>
+                        3FZbgi29cpjq2GjdwV8eyrt4
+                      </Text>
+                      <Text
+                        fz={10}
+                        c="#FFC107"
+                        bg="#FFF4E0"
+                        ta="center"
+                        w={54}
+                        style={{ borderRadius: "5px" }}
+                      >
+                        Pending
+                      </Text>
+                    </Box>
+                  </Flex>
                 </Box>
+                <Text>0.015 BTC</Text>
               </Flex>
             </Box>
             <Box
@@ -178,57 +187,30 @@ const Transaction = () => {
               }}
             >
               <Text ta="left" fz={10} c="#3d3b3c">
-                #2-8/28/2024 at 12:40
+                #4-8/28/2024 at 16:45
               </Text>
-              <Flex justify="space-between" align="center" gap={130}>
-                <Flex justify="center" align="center" gap={20}>
-                  <MdOutlineCurrencyExchange fontSize={30} color="#ff7f11" />
-                  <Box>
-                    <Text fz={16}>Execute</Text>
-                    <Text
-                      fz={10}
-                      c="#E84F1F"
-                      bg="#F5F2E0"
-                      pl={5}
-                      style={{ borderRadius: "5px" }}
-                    >
-                      Approved
-                    </Text>
-                  </Box>
-                </Flex>
+              <Flex justify="space-between" align="center" gap={73}>
                 <Box>
-                  <Text>$ 0.00 USD</Text>
+                  <Flex align="center" gap={20}>
+                    <FaEthereum fontSize={30} color="#627eea" />
+                    <Box>
+                      <Text fz={10} mb={5}>
+                        0x6fAB3c94A3eC993092D9
+                      </Text>
+                      <Text
+                        fz={10}
+                        c="#E74C3C"
+                        bg="#FDEDEC"
+                        ta="center"
+                        w={54}
+                        style={{ borderRadius: "5px" }}
+                      >
+                        Rejected
+                      </Text>
+                    </Box>
+                  </Flex>
                 </Box>
-              </Flex>
-            </Box>
-            <Box
-              style={{
-                borderTop: "1px solid #b7d1da",
-                marginBottom: "10px",
-              }}
-            >
-              <Text ta="left" fz={10} c="#3d3b3c">
-                #2-8/28/2024 at 12:40
-              </Text>
-              <Flex justify="space-between" align="center" gap={130}>
-                <Flex justify="center" align="center" gap={20}>
-                  <MdOutlineCurrencyExchange fontSize={30} color="#ff7f11" />
-                  <Box>
-                    <Text fz={16}>Execute</Text>
-                    <Text
-                      fz={10}
-                      c="#E84F1F"
-                      bg="#F5F2E0"
-                      pl={5}
-                      style={{ borderRadius: "5px" }}
-                    >
-                      Approved
-                    </Text>
-                  </Box>
-                </Flex>
-                <Box>
-                  <Text>$ 0.00 USD</Text>
-                </Box>
+                <Text>0.048 ETH</Text>
               </Flex>
             </Box>
           </Flex>
